c1278e4aecbcb6b1afdb6eaaef250f0861f6ab77
tgadus <tgadus@iu.edu>
2025-09-29 15:36:59 -0400

    Add files via upload

---
+++ M5_Listing2.9.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ComputeLoan {
+  public static void main(String[] args) {   
+    // Create a Scanner
+    Scanner input = new Scanner(System.in);
+
+    // Enter yearly interest rate
+    System.out.print("Enter annual interest rate, e.g., 7.25: ");
+    double annualInterestRate = input.nextDouble();
+    
+    // Obtain monthly interest rate
+    double monthlyInterestRate = FILL_CODE_OR_CLICK_ANSWER
+
+    // Enter number of years
+    System.out.print(
+      "Enter number of years as an integer, e.g., 5: ");
+    int numberOfYears = input.nextInt();
+    
+    // Enter loan amount
+    System.out.print("Enter loan amount, e.g., 120000.95: ");
+    double loanAmount = input.nextDouble();
+    
+    // Calculate payment
+    double monthlyPayment = loanAmount * monthlyInterestRate / (1
+      - 1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12));
+    double totalPayment = FILL_CODE_OR_CLICK_ANSWER
+
+    // Display results
+    System.out.println("The monthly payment is $" + 
+      (int)(monthlyPayment * 100) / 100.0);
+    System.out.println("The total payment is $" + 
+      (int)(totalPayment * 100) / 100.0);
+  }
+}
